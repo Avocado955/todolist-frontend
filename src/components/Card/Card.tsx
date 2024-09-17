d91a6e4a89a2d13f86bdd9d6992bf1600bb6b390
Avocado955 <mellis955@gmail.com>
2024-09-17 11:36:41 +1000

    adds an extra dialog box which opens the edit tab for the todos

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,8 @@
 import styles from "./Card.module.scss";
 import { ToDoListResponse, updateToDoById } from "../../services/todoservices";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ToDoListForm from "../ToDoListForm/ToDoListForm";
+import { ToDoListData } from "../ToDoListForm/schema";
 
 
 interface CardProps {
@@ -8,7 +10,13 @@ interface CardProps {
 }
 
 const Card = ({todo}:CardProps) => {
+  const [todoDialog, setTodoDialog] = useState<HTMLDialogElement | null>(null);
   const [checkedState, setCheckedState] = useState<boolean>(Boolean(todo.isCompleted));
+
+  useEffect(() => {
+    const foundTodoDialog = document.getElementById('todoFormEdit') as HTMLDialogElement;
+    setTodoDialog(foundTodoDialog);
+  }, [])
 
   const valueChanged = () => {
     const isCompleteValue = !checkedState;
@@ -16,6 +24,22 @@ const Card = ({todo}:CardProps) => {
     const newTodo = {task: todo.task, categoryId: todo.category.id.toString(), isCompleted: isCompleteValue};
     console.log(newTodo);
     updateToDoById(todo.id, newTodo).then(result => console.log(result)).catch(e => console.log(e));
+  }
+
+  const onToDoSubmit = (data: ToDoListData) => {
+    console.log(data);
+  }
+  
+  const openTodoDialog = () => {
+    if (todoDialog?.open) {
+      todoDialog?.close();
+    } else {
+      todoDialog?.showModal();
+    }
+  }
+
+  const closeTodoDialog = () => {
+    todoDialog?.close();
   }
 
   return (
@@ -29,9 +53,13 @@ const Card = ({todo}:CardProps) => {
         </div>
       </div>
       <div className={styles.sectionColumn}>
-        <button className={styles.editBtn}>Edit</button>
+        <button className={styles.editBtn} onClick={openTodoDialog}>Edit</button>
         <button className={styles.duplicateBtn}>Duplicate</button>
       </div>
+      <dialog id="todoFormEdit" className={styles.modal} >
+        <ToDoListForm onSubmit={onToDoSubmit} mode="EDIT"></ToDoListForm>
+        <button className={styles.modal_close} onClick={closeTodoDialog}>X</button>
+      </dialog>
     </div>
   )
 }
