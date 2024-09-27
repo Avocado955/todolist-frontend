54d55afcaab466444d1302320e2eea27be6fe2e7
Avocado955 <mellis955@gmail.com>
2024-09-27 11:08:09 +1000

    adds the context provider for Todo and Category

---
+++ src/contexts/CategoryContextProvider.tsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, useState } from "react";
+import { CategoryData } from "../components/CategoryForm/schema";
+import { CategoryResponse, getAllCategories } from "../services/categoryservice";
+
+interface CategoryContextValues {
+  categoryData: CategoryResponse[];
+  onCategoryCreate: (data: CategoryData) => Promise<unknown>;
+  onCategoryDelete: (id: string) => Promise<unknown>;
+}
+
+export const CategoryContext = createContext<CategoryContextValues>({
+  categoryData: [],
+  onCategoryCreate: async (data: CategoryData) => console.log(data),
+  onCategoryDelete: async (id: string) => console.log(id),
+});
+
+interface CategoryContextProviderProps {
+  children?: React.ReactNode;
+}
+
+const CategoryContextProvider = ({children} : CategoryContextProviderProps) => {
+  const [categoryData, setCategoryData] = useState<CategoryResponse[]>([]);
+
+  useEffect(() => {
+    getAllCategories().then((data) => setCategoryData(data));
+  }, []);
+
+  const onCategoryCreate = async (data: CategoryData) => {
+
+  };
+
+  const onCategoryDelete = async (id: string) => {
+
+  };
+
+  return (
+    <CategoryContext.Provider
+      value={{categoryData, onCategoryCreate, onCategoryDelete}}
+    >
+      {children}
+    </CategoryContext.Provider>
+  )
+}
+
+export default CategoryContextProvider;
--- src/contexts/TodoContextProvider.tsx
+++ src/contexts/TodoContextProvider.tsx
@@ -1,7 +1,45 @@
-// import { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { getAllToDos, ToDoListResponse } from "../services/todoservices";
+import { ToDoListData } from "../components/ToDoListForm/schema";
 
-// interface TodoContextValues {
-  
-// }
+interface TodoContextValues {
+  todoData: ToDoListResponse[];
+  onTodoCreate: (data: ToDoListData) => Promise<unknown>;
+  onTodoDelete: (id: string) => Promise<unknown>;
+}
 
-// export const TodoContext = createContext<
+export const TodoContext = createContext<TodoContextValues>({
+  todoData: [],
+  onTodoCreate: async (data: ToDoListData) => console.log(data),
+  onTodoDelete: async (id: string) => console.log(id),
+});
+
+interface TodoContextProviderProps {
+  children?: React.ReactNode;
+}
+
+const TodoContextProvider = ({children} : TodoContextProviderProps) => {
+  const [todoData, setTodoData] = useState<ToDoListResponse[]>([]);
+
+  useEffect(() => {
+    getAllToDos().then((data) => setTodoData(data));
+  }, []);
+
+  const onTodoCreate = async (data: ToDoListData) => {
+
+  };
+
+  const onTodoDelete = async (id: string) => {
+
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{todoData, onTodoCreate, onTodoDelete}}
+    >
+      {children}
+    </TodoContext.Provider>
+  )
+}
+
+export default TodoContextProvider;
